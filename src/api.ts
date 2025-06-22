a6693aa33b3a2c39b0c2e75ec4552e473889fcb7
Ahmet Abdullah Gültekin <ahmetabdullahgultekin@gmail.com>
2025-06-23 02:33:32 +0300

    Update API base URL to use HTTPS for improved security

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://13.60.65.245:8080', // Uzak sunucu adresi güncellendi
+  baseURL: 'https://13.60.65.245:8080', // Uzak sunucu adresi güncellendi
   withCredentials: true // Gerekirse çerez desteği
 });
 
